438f25a0b18347ef380803dc04a4b3e2e23d1ca0
RajatSaini04 <rajatsaini0411@gmail.com>
2024-08-30 22:38:32 +0530

    js update

--- Src/Netflix-Script.js
+++ Src/Netflix-Script.js
@@ -1,1 +1,14 @@
 // FAQ 
+
+let faq = document.querySelectorAll(".faq")
+
+
+faq.forEach(faq =>{
+
+    const answer = faq.querySelector(".faq-answer")
+
+    faq.addEventListener("click", () =>{
+
+        answer.classList.toggle("active")
+    })
+})
