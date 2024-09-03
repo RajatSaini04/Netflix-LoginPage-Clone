badf51d8c38176620997e466d547678e5cb5e94b
RajatSaini04 <rajatsaini0411@gmail.com>
2024-09-03 13:43:58 +0530

    Js-upadte

--- Src/Netflix-Script.js
+++ Src/Netflix-Script.js
@@ -1,14 +1,26 @@
-// FAQ 
+// // FAQ 
 
-let faq = document.querySelectorAll(".faq")
+let faq = document.querySelectorAll(".faq-item")
 
+faq.forEach(faqitem => {
 
-faq.forEach(faq =>{
+    const answer = faqitem.querySelector(".faq-answer")
+    const icon = faqitem.querySelector(".icon-plus")
+    const icon2 = faqitem.querySelector(".icon-plus2")
 
-    const answer = faq.querySelector(".faq-answer")
+    faqitem.addEventListener("click", (event) => {
+        event.stopPropagation(); // Prevent this click from propagating to the document
 
-    faq.addEventListener("click", () =>{
+        faq.forEach(item => {
+            if (item !== faqitem) {
+                item.querySelector(".faq-answer").classList.remove("active")
+                item.querySelector(".icon-plus").classList.remove("active")
+                item.querySelector(".icon-plus2").classList.remove("active")
+            }
+        });
 
         answer.classList.toggle("active")
+        icon.classList.toggle("active")
+        icon2.classList.toggle("active")
     })
-})
+});
